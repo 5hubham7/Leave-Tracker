b40ffe29ca2996d4450f0c872e83cb3095cb27e3
Shubham S. Yadav <5hubham7adav@gmail.com>
2021-02-22 23:30:07 +0530

    updated error messages

--- server/_helpers/error-handler.js
+++ server/_helpers/error-handler.js
@@ -1,17 +1,17 @@
 function errorHandler(err, req, res, next) {
     if (typeof err === "string") {
-        return res.json({ status: 400, error: err });
+        return res.json({ status: 400, error: err, response: null });
     }
 
     if (err.name === "ValidationError") {
-        return res.json({ status: 400, error: err.message });
+        return res.json({ status: 400, error: err.message, response: null });
     }
 
     if (err.name === "UnauthorizedError") {
-        return res.json({ status: 401, error: err.message });
+        return res.json({ status: 401, error: err.message, response: null });
     }
 
-    return res.json({ status: 500, error: err.message });
+    return res.json({ status: 500, error: err.message, response: null });
 }
 
 module.exports = errorHandler;
